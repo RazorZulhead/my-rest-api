063e55e62f39a542ba415c8454aa00c03ba18e74
RazorZulhead <166948283+RazorZulhead@users.noreply.github.com>
2024-04-14 03:46:22 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'RazorZulhead'// yourname
+global.MONGO_DB_URI = "mongodb+srv://razorvpnstore:kVlhmb5XIvumZ2qu@cluster0.bkyrqjo.mongodb.net/" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "razorvpnstore@gmail.com" //email
+global.email_password = "hjph ornk tfcg clca" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
